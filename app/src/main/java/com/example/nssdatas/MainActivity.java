d0802a7675dbfb2c54ebc60ac88b2208de67bd04
Amartya Mondal <hayyoulistentome@gmail.com>
2019-05-10 00:42:40 +0530

    Icons changed

--- app/src/main/java/com/example/nssdatas/MainActivity.java
+++ app/src/main/java/com/example/nssdatas/MainActivity.java
@@ -6,11 +6,8 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.view.ContextMenu;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;

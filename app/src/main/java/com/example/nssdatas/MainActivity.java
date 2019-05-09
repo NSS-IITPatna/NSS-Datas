44e70cf2908f610b3f8e32f5a3e5fb5e6ed906eb
Amartya Mondal <hayyoulistentome@gmail.com>
2019-05-10 00:10:58 +0530

    Signout option added

--- app/src/main/java/com/example/nssdatas/MainActivity.java
+++ app/src/main/java/com/example/nssdatas/MainActivity.java
@@ -6,7 +6,11 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
@@ -87,6 +91,23 @@ public class MainActivity extends AppCompatActivity{
         super.onPause();
         if (mAuthStateListener != null) {
             mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_item, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sign_out:
+                AuthUI.getInstance().signOut(this);
+                return true;
+            default:
+                return super.onContextItemSelected(item);
         }
     }
 

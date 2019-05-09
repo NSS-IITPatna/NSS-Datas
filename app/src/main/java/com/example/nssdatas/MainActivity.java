fb4770512ae64e0b938a8b08e688b228863c0dc3
Amartya Mondal <hayyoulistentome@gmail.com>
2019-05-09 11:54:31 +0530

    Added the 3 fragments

--- app/src/main/java/com/example/nssdatas/MainActivity.java
+++ app/src/main/java/com/example/nssdatas/MainActivity.java
@@ -3,13 +3,30 @@ package com.example.nssdatas;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     private TextView mTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ThankFragment thankFragment=new ThankFragment();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.parent_container,thankFragment).commit();
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,27 +35,35 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_think:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    doThink();
                     return true;
                 case R.id.navigation_thank:
-                    mTextMessage.setText(R.string.title_home);
+                    doThank();
                     return true;
                 case R.id.navigation_blood:
-                    mTextMessage.setText(R.string.title_notifications);
+                    doBlood();
                     return true;
             }
             return false;
         }
     };
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    private void doThank() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.parent_container, new ThankFragment());
+        ft.commit();
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    private void doThink() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.parent_container, new ThinkFragment());
+        ft.commit();
+    }
+
+    private void doBlood() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.parent_container, new BloodRequestFragment());
+        ft.commit();
+    }
 }
